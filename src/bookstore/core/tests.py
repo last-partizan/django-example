f58ba3fbd08393effffbf0e2e176d0efa80307ff
Serhii Tereshchenko <serg.partizan@gmail.com>
2023-05-23 09:21:23 +0300

    update: Django default language error

--- src/bookstore/core/tests.py
+++ src/bookstore/core/tests.py
@@ -1,47 +1,21 @@
 import pytest
-from pytest_lambda import lambda_fixture
-from pytest_cases import fixture_ref, pytest_parametrize_plus
 
-from . import models
+from django.utils import translation
 
 
-def lazy_fixture_generator(mod):
-    """
-    Lazy = lazy_fixture_generator(globals())
-    """
-    ids = (x for x in range(1000))
+@pytest.mark.parametrize(
+    "language,expected",
+    [("en", "Yes"), ("de", "Ja")],
+)
+def test_activate_language(language, expected):
+    translation.activate(language)
 
-    def Lazy(fun):
-        """
-        Lazy fixture loading from lambda
+    result = translation.gettext("Yes")
 
-        Usage:
-        @pytest_parametrize_plus("args", [Lazy(lambda f1, f2: (f1.atrr, f2.attr))])
-        """
-        name = f"lazy{next(ids)}"
-        mod[name] = lambda_fixture(fun)
-        return fixture_ref(name)
-    return Lazy
+    assert result == expected
 
 
-Lazy = lazy_fixture_generator(globals())
+def test_default_translation():
+    result = translation.gettext("Yes")
 
-
-@pytest.fixture
-def book1():
-    return models.Book(name="Moby Dick")
-
-
-@pytest.fixture
-def book2():
-    return models.Book(name="Alice in Wonderland")
-
-
-@pytest_parametrize_plus("name, expects", [
-    (Lazy(lambda book1: book1.name), "Moby Dick"),
-    (Lazy(lambda book2: book2.name), "Alice in Wonderland"),
-])
-@pytest.mark.django_db
-def test_get_or_create_book(name, expects):
-    book = models.Book.objects.get_or_create(name=name)[0]
-    assert book.name == expects
+    assert result == "Yes"
